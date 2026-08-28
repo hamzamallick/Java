de401ff33547a6ea3750c207d7a3de57cc859029
hamzamallick <mohdhamza1928@gmail.com>
2026-08-28 20:13:39 +0530

    Initial commit

---
+++ src/basics/Main.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static void main() {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        var name = input.nextLine();
+
+        System.out.println("My name is "+ name);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/basics/Primitives.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Primitives {
+    static void main() {
+
+        // Primitives data are those which can not be further breakable like int (it can not be breakable) but String can
+        // further be breakable into char
+
+
+
+    }
+}
